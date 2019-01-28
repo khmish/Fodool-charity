4f2f318d2537fa3e9f0e36a76445ab1a2e08b274
khmish <hk0021@gmail.com>
2019-01-28 16:07:49 +0300

    Create dependencies

--- app/comment.php
+++ app/comment.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     //
-    public function user()
+    public function Muser()
     {
-        return $this->belongsTo('App\User', 'foreign_key', 'other_key');
+        return $this->belongsTo('App\User', 'user_id', 'id');
     }
-    public function user()
+    public function Msubject()
     {
-        return $this->belongsTo('App\User', 'foreign_key', 'other_key');
+        return $this->belongsTo('App\Subject', 'blog_id', 'id');
     }
 }
